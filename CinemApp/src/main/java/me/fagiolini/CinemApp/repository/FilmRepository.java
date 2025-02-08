4bac3e92b632a8bb4653dee6253d5d9537aeb798
Daniele Fagiolini <daniele@fagiolini.me>
2025-02-08 08:56:10 +0100

    Una query in JPQL che funziona, stranamente

--- CinemApp/src/main/java/me/fagiolini/CinemApp/repository/FilmRepository.java
+++ CinemApp/src/main/java/me/fagiolini/CinemApp/repository/FilmRepository.java
@@ -6,14 +6,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import me.fagiolini.CinemApp.model.Film;
+import org.springframework.data.repository.query.Param;
 
 public interface FilmRepository extends JpaRepository<Film, Long> {
     
     /* @Query("SELECT DISTINCT f FROM Proiezione p JOIN p.film f JOIN p.sala s JOIN s.cinema c WHERE c.id = ?1")
     List<Film> findFilmByCinema(long cinemaId); */
     
-    @Query(value = "select f.* from proiezione p , cinema c , cinema_sale cs , sala s , film f where c.id = cs.cinema_id and s.id = cs.sale_id and p.sala_id = s.id and f.id = p.film_id and c.id = ?1;", nativeQuery = true)
-    List<Film> findFilmByCinema(long cinemaId);
-    
+//    @Query(value = "select f.* from proiezione p , cinema c , sala s , film f where c.id = s.cinema_id and p.sala_id = s.id and f.id = p.film_id and c.id = ?1;", nativeQuery = true)
+//    List<Film> findFilmByCinema(long cinemaId);
+
+    @Query("SELECT DISTINCT f FROM Proiezione p JOIN p.film f JOIN p.sala s JOIN s.cinema c WHERE c.id = :cinemaId")
+    List<Film> findFilmByCinema(@Param("cinemaId") long cinemaId);
 
 }
